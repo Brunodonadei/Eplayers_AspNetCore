5f8e661409f8900fb24aa2077f2888b5822865cd
Bruno Donadei <domanbruno@gmail.com>
2021-01-18 10:30:36 -0300

    adicionando o jogador

--- Interfaces/IEquipe.cs
+++ Interfaces/IEquipe.cs
@@ -5,8 +5,8 @@ namespace Eplayers_AspNetCore.Interfaces
 {
     public interface IEquipe
     {
-         void Create(Equipe e);
-         List<Equipe> ReadAll();
+        void Create(Equipe e);
+        List<Equipe> ReadAll();
         void Update(Equipe e);
         void Delete(int id);
     }
--- Models/Jogador.cs
+++ Models/Jogador.cs
@@ -1,9 +1,70 @@
+using System.Collections.Generic;
+using System.IO;
+using Eplayers_AspNetCore.Interfaces;
+
 namespace Eplayers_AspNetCore.Models
 {
-    public class Jogador
+    public class Jogador : EPlayersBase, IJogador
     {
-        public int MyProperty { get; set; }
+        public int IdJogador { get; set; }
         public string Nome { get; set; }
         public int IdEquipe { get; set; }
+        private const string PATH = "Database/Jogador.csv";
+
+        public Jogador()
+        {
+            CreateFolderAndFile(PATH);
+        }
+
+        public string Prepare(Jogador j)
+        {
+            return $"{j.IdJogador};{j.Nome};{j.IdEquipe}";
+        }
+
+        public void Create(Jogador j)
+        {
+            string[] linhas = { Prepare(j) };
+            File.AppendAllLines(PATH, linhas);
+        }
+
+        public List<Jogador> ReadAll()
+        {
+            List<Jogador> jogadores = new List<Jogador>();
+            string[] linhas = File.ReadAllLines(PATH);
+
+            foreach (var item in linhas)
+            {
+                string[] linha = item.Split(";");
+
+                Jogador jogador = new Jogador();
+                jogador.IdJogador = int.Parse(linha[0]);
+                jogador.Nome = linha[1];
+                jogador.IdEquipe = int.Parse(linha[2]);
+
+                jogadores.Add(jogador);
+            }
+            return jogadores;
+
+        }
+
+        public void Update(Jogador j)
+        {
+            List<string> linhas = ReadAllLinesCSV(PATH);
+
+            linhas.RemoveAll(x => x.Split(";")[0] == j.IdJogador.ToString());
+
+            linhas.Add(Prepare(j));
+
+            RewriteCSV(PATH, linhas);
+        }
+
+        public void Delete(int id)
+        {
+            List<string> linhas = ReadAllLinesCSV(PATH);
+
+            linhas.RemoveAll(x => x.Split(";")[0] == id.ToString());
+
+            RewriteCSV(PATH, linhas);
+        }
     }
 }
